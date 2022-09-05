c8f22029d633c9979502132aa8ea4f4314e3985d
evreekum <e.v.reekum@gmail.com>
2022-09-05 18:01:24 +0200

    boilerplate chapter 3 React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>State uitgelegd!</h1>
+
+      <h3>De gekozen kleur is:</h3>
+
+      <button type="button" className="purple">
+        Paars
+      </button>
+      <button type="button" className="red">
+        Rood
+      </button>
+      <button type="button" className="pink">
+        Roze
+      </button>
     </div>
   );
 }
